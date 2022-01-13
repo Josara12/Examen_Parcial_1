dd01277b2a5e3a02f65ae68d6a37aa4f9186df4a
Josara12 <sarai_nj27@hotmail.com>
2022-01-13 13:19:46 -0500

    Resuelto el literal 4

--- ejercicio_1101.sh
+++ ejercicio_1101.sh
@@ -8,12 +8,19 @@ pwd
 echo ¿Cuál es el tamaño del archivo Marra2014_data.fasta?
 df -h ../data/Marra2014_data.fasta 
 
-
+#Literal 3
 cp ../data/Marra2014_data.fasta . 
 cp Marra2014_data.fasta my_file.fasta
 
 rm Marra2014_data.fasta
 
- 
+#Literal 4 
+echo ¿Cuántos contigs están clasificados como isogroup00036?
+a=`grep -c isogroup00036 my_file.fasta`
+echo contigs $a
+
+
+
+  
 
 
